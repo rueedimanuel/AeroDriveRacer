2d888f121f441cc9f66d959f5cb250cf0a1ac7df
Manuel <m4nuel.r@gmail.com>
2023-12-18 18:07:50 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include "DistanceSensor.h"
 
-// put function declarations here:
+// Define the trigger pin
+#define TRIGGER_PIN 13
 
-void setup() {
-  // put your setup code here, to run once:
+// Create instances of DistanceSensor
+DistanceSensor DistanceSensorL(10, TRIGGER_PIN); // Left sensor
+DistanceSensor DistanceSensorR(11, TRIGGER_PIN); // Right sensor
+DistanceSensor DistanceSensorC(12, TRIGGER_PIN); // Center sensor
+
+void setup()
+{
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print(1);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+void loop()
+{
+  DistanceSensorL.updateDistance();
+  DistanceSensorR.updateDistance();
+  DistanceSensorC.updateDistance();
+
+  Serial.print("Left Sensor: ");
+  Serial.print(DistanceSensorL.getDistance());
+  Serial.print(" cm, Right Sensor: ");
+  Serial.print(DistanceSensorR.getDistance());
+  Serial.print(" cm, Center Sensor: ");
+  Serial.print(DistanceSensorC.getDistance());
+  Serial.println(" cm");
+
+  delay(10); // Minimal delay to avoid serial buffer overflow
 }
-
-
-// put function definitions here:
